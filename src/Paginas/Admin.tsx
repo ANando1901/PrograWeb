94ddb8a7b65c09f9636717586831ab97881b920d
tornoa2 <20183261@aloe.ulima.edu.pe>
2025-05-22 10:45:40 -0500

    Update Admin.tsx

--- src/Paginas/Admin.tsx
+++ src/Paginas/Admin.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
 import Sidebar from "../Componentes/Barra";
 import { Table } from "../Componentes/Tabla";
-import "../Estilos/vistaAdmin.css"
-import { ListaGames } from "../Utils/ListaJuegos";
-
-
-import "../Estilos/Modal.css"
+import NavBar from "../Componentes/BarraNavegacion";
+import "../Estilos/vistaAdmin.css";
+import "../Estilos/Modal.css";
 
 export default function VistaAdmin() {
   return (
-    <div className="layout-container modal-container">
-      <Sidebar/>
-      <Table/>
+    <div>
+      <NavBar />
+      <div className="d-flex" style={{ marginTop: "60px" }}>
+        <Sidebar />
+        <main className="p-4 w-100">
+          <h2>Gestor de Juegos</h2>
+          <div className="card-body">
+            <div className="table-responsive">
+              <Table />
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
